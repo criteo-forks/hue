c1ea90886991db1cad02f5a4d5a60c548d5e29c0
a.mazurov <a.mazurov@criteo.com>
2019-12-10 10:51:24 +0100

    Close all user sessions on logout
    Fix the issue when logout does not close sessions like vertica, presto
    
    The logic is the following - get all registred interpreters for the user,
    retrive API and call close_session method.

--- desktop/libs/notebook/src/notebook/api.py
+++ desktop/libs/notebook/src/notebook/api.py
@@ -244,7 +244,7 @@ def fetch_result_data(request):
   response['result'] = get_api(request, snippet).fetch_result(notebook, snippet, rows, start_over)
 
   # Materialize and HTML escape results
-  if response['result'].get('data') and response['result'].get('type') == 'table' and not response['result'].get('isEscaped'):
+  if response['result'] and response['result'].get('data') and response['result'].get('type') == 'table' and not response['result'].get('isEscaped'):
     response['result']['data'] = escape_rows(response['result']['data'])
     response['result']['isEscaped'] = True
 
